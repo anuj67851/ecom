7a84801a3ed0034124ca1ec018d413135404c6fb
Anuj Patel <47494775+anuj67851@users.noreply.github.com>
2019-08-27 22:52:11 +0530

    Update config.php

--- resources/config.php
+++ resources/config.php
@@ -14,7 +14,7 @@ defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK",__DIR__.DS."templates/b
 
 defined("DB_HOST") ? null : define("DB_HOST","localhost");
 defined("DB_USER") ? null : define("DB_USER","root");
-defined("DB_PASS") ? null : define("DB_PASS","anuj3108");
+defined("DB_PASS") ? null : define("DB_PASS","");
 defined("DB_NAME") ? null : define("DB_NAME","ecom_db");
 
 $connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
